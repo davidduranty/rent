6bf01ae47e90997a6549557d66c3d66757bf3c38
Nassitch <Nassitch@hotmail.fr>
2025-05-07 17:46:40 +0200

    feat: add reservation form component and configure routing

---
+++ frontend/src/app/home/reservation-form/reservation-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-reservation-form',
+  imports: [],
+  templateUrl: './reservation-form.component.html',
+  styleUrl: './reservation-form.component.css'
+})
+export class ReservationFormComponent {
+
+}
