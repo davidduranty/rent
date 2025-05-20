4ebfd2139269573a21c94e130e4a3d665b29b9f7
Nassitch <Nassitch@hotmail.fr>
2025-05-20 16:19:54 +0200

    Front: Use method from vehicle service for search vehicle with special parmas.

--- frontend/src/app/home/reservation-form/reservation-form.component.ts
+++ frontend/src/app/home/reservation-form/reservation-form.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { dateIsTodayOrLater, endDateAfterStartDate, requiredValidator } from '../../utils/validators';
+import {Component, inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {dateIsTodayOrLater, endDateAfterStartDate, requiredValidator} from '../../utils/validators';
+import {VehicleService} from '../../services/vehicle.service';
+import {SearchParams} from '../../models/search-params.model';
 
 export interface Reservation {
   isProfessional: boolean;
@@ -17,8 +19,11 @@ export interface Reservation {
   imports: [ReactiveFormsModule]
 })
 export class ReservationFormComponent implements OnInit {
-  private fb: FormBuilder = inject(FormBuilder);
-  reservationForm!: FormGroup;
+
+  private fb: FormBuilder = inject<FormBuilder>(FormBuilder);
+  private vehicleService: VehicleService = inject<VehicleService>(VehicleService);
+
+  protected reservationForm!: FormGroup;
 
   ngOnInit(): void {
     this.reservationForm = this.fb.group({
@@ -26,7 +31,7 @@ export class ReservationFormComponent implements OnInit {
       location: ['', requiredValidator()],
       startDate: [null, [requiredValidator(), dateIsTodayOrLater()]],
       endDate: [null, requiredValidator()]
-    }, { validators: endDateAfterStartDate() });
+    }, {validators: endDateAfterStartDate()});
   }
 
   onSubmit() {
@@ -37,9 +42,18 @@ export class ReservationFormComponent implements OnInit {
       console.log('Formulaire invalide');
       this.reservationForm.markAllAsTouched();
     }
+
+    const params: SearchParams = {
+      isProfessional: this.reservationForm.get('isProfessional')?.value,
+      location: this.reservationForm.get('location')?.value,
+      startDate: this.reservationForm.get('startDate')?.value,
+      endDate: this.reservationForm.get('endDate')?.value
+    }
+
+    this.vehicleService.getVehicleByParma(params);
   }
 
   setProfessional(value: boolean) {
-    this.reservationForm.patchValue({ isProfessional: value });
+    this.reservationForm.patchValue({isProfessional: value});
   }
 }
