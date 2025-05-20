73c9619400c4f6b8b2ff9b787484e3a3fcf780eb
Nassitch <Nassitch@hotmail.fr>
2025-05-20 16:03:17 +0200

    Back: Update migrations.

--- backend/src/migrations/Migration20250505081804.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20250505081804 extends Migration {
-
-  override async up(): Promise<void> {
-    this.addSql(`create table "rentACar"."user" ("id" serial primary key, "name" varchar(255) not null, "surname" varchar(255) not null, "email" varchar(255) not null, "password" varchar(255) not null, "birthday" date not null, "is_admin" boolean not null, "profesionnal" boolean not null);`);
-  }
-
-}
