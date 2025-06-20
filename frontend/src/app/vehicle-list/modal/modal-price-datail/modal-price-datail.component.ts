df0ac3ce53d9ce9f7738b2a6c2a482af6c5d5499
David Duranty <dadaduranty@gmail.com>
2025-06-20 13:02:31 +0200

    feat(modal): ajouter un nouveau modal pour afficher les détails du prix et mettre à jour la navigation

---
+++ frontend/src/app/vehicle-list/modal/modal-price-datail/modal-price-datail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-price-datail',
+  imports: [],
+  templateUrl: './modal-price-datail.component.html',
+  styleUrl: './modal-price-datail.component.css'
+})
+export class ModalPriceDatailComponent {
+
+}
